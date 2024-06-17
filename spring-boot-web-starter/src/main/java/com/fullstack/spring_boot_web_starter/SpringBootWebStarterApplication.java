a95689dc9fb19a92d951de804fd43bda190d5180
vvskchaitanya <vvskchaitanya@gmail.com>
2024-06-17 00:34:36 +0000

    spring-boot-web-starter

---
+++ spring-boot-web-starter/src/main/java/com/fullstack/spring_boot_web_starter/SpringBootWebStarterApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack.spring_boot_web_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebStarterApplication.class, args);
+	}
+
+}
---
+++ spring-boot-web-starter/src/test/java/com/fullstack/spring_boot_web_starter/SpringBootWebStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.spring_boot_web_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
