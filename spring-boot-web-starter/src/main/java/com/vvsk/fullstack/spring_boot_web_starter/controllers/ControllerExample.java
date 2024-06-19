01b3b7c1ddf452e746b7d9d928ab60a67c225972
vvskchaitanya <vvskchaitanya@gmail.com>
2024-06-19 03:37:24 +0000

    unsused dep

--- spring-boot-web-starter/src/main/java/com/vvsk/fullstack/spring_boot_web_starter/controllers/ControllerExample.java
+++ spring-boot-web-starter/src/main/java/com/vvsk/fullstack/spring_boot_web_starter/controllers/ControllerExample.java
@@ -3,8 +3,6 @@ package com.vvsk.fullstack.spring_boot_web_starter.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class ControllerExample {
